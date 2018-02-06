a1470e4beb4248fe4927d62e7c7b6a654c52ed02
github username <github email address>
2018-02-06 11:45:25 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,17 +5,16 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: {in: %w{Fiction Non-Fiction}}
   validate :is_clickbait?
 
-  CLICKBAIT_PATTERNS = [
-    /Won't Believe/i,
-    /Secret/i,
-    /Top [0-9]*/i,
-    /Guess/i
-  ]
+
+
 
   def is_clickbait?
-    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
-      errors.add(:title, "must be clickbait")
-    end
-  end
+    return false if title == nil
+    cb = ["Won't Believe", "Secret", "Top 0", "Top 1", "Top 2", "Top 3", "Top 4", "Top 5", "Top 6", "Top 7", "Top 8", "Top 9", "Top 10", "Guess"]
+    catch = []
+    title_str = title.scan(/\S/).join('')
+    cb.each{|phrase| catch << phrase if title_str.include?(phrase.gsub(" ",""))}
+    errors.add(:title, "must be clickbait") if catch.empty?
+end
 
 end
